854ba8757ff270d4ae995178b6f3256f64d60969
Nuno Silva <craked5@gmail.com>
2018-06-26 01:47:55 +0100

    Added unit tests

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from flask import Flask, request
-import json
-import re
+from search import AutocompleteSearch
 import csv
+import json
 
-with open('test_files/190titles.csv', 'rb') as csv_file:
-    titles_list = [i[0] for i in csv.reader(csv_file, delimiter=' ', quotechar='|')]
 app = Flask(__name__)
+trie = AutocompleteSearch()
 
-app.config['DEBUG'] = True
+with open('test_files/6500titles.csv', 'r') as csv_file:
+    title_list = [i[0] for i in csv.reader(csv_file)]
+for title in title_list:
+    trie.insert(title)
 
-@app.route('/values', methods=["Post"])
+
+@app.route('/autocomplete', methods=["Post"])
 def get_values():
-    beginning = request.get_data()
+    search_result = trie.autocomplete_start(request.get_data())
+    if search_result is None or search_result is []:
+        return json.dumps("No matching titles.")
+    return json.dumps(search_result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8080, threaded=True)
---
+++ test_search.py
@@ -0,0 +1,40 @@
+import unittest
+import csv
+import search
+import requests
+
+
+class SearchTests(unittest.TestCase):
+    with open('test_files/6500titles.csv', 'r') as csv_file:
+        little_list = [i[0] for i in csv.reader(csv_file)]
+    trie = search.AutocompleteSearch()
+    for title in little_list:
+        trie.insert(title)
+
+    def test_single_existing(self):
+        self.assertEqual(self.trie.autocomplete_start("Facebook Lite"), ["Facebook Lite"])
+
+    def test_multiple_existing(self):
+        self.assertEqual(self.trie.autocomplete_start("Face"), ["FaceApp", "Face Swap", "Face Changer",
+                                                                "Face Changer 2", "Face Time Calling Guide",
+                                                                "Facebook", "Facebook Apps Market",
+                                                                "Facebook Pages Manager", "Facebook Lite",
+                                                                "Facebook Groups", "Facebook Video Download",
+                                                                "FaceLOOK for Facebook", "FaceLock for apps",
+                                                                "FaceSwap Face Swap Live", "Facetune"])
+
+    def test_non_existing(self):
+        self.assertEqual(self.trie.autocomplete_start("dawfsegsrda"), None)
+
+    def test_get_request(self):
+        res = requests.get("http://localhost:8080/autocomplete", timeout=2)
+        self.assertEqual(res.status_code, 405)
+
+    def test_input_int(self):
+        self.assertRaises(TypeError, self.trie.autocomplete_start, 444)
+
+    def test_input_float(self):
+        self.assertRaises(TypeError, self.trie.autocomplete_start, 1.23242)
+
+    def test_insert_int(self):
+        self.assertRaises(TypeError, self.trie.insert, 444)
